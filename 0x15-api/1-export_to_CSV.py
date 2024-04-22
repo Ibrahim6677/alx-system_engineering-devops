c7cae37f6a7911ebf447e051b7c0838e853e9d1d
ibrahim <ibrahimsaad6677@gmail.com>
2024-04-22 15:33:38 -0700

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
-"""
-Script to retrieve and export information about an employee's TODO list progress in CSV format.
-"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 
 import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
+    # Get the user ID from the command-line arguments provided to the script
+    user_id = sys.argv[1]
 
-    employee_id = int(sys.argv[1])
+    # Define the base URL for the JSON API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
-    todo_response = requests.get("{}/todos?userId={}".format(base_url, employee_id))
+    # Fetch user information from the API and
+    #   convert the response to a JSON object
+    user = requests.get(url + "users/{}".format(user_id)).json()
 
-    if user_response.status_code != 200:
-        print("Error: Could not retrieve user data")
-        sys.exit(1)
+    # Extract the username from the user data
+    username = user.get("username")
 
-    if todo_response.status_code != 200:
-        print("Error: Could not retrieve TODO list data")
-        sys.exit(1)
+    # Fetch the to-do list items associated with the
+    #   given user ID and convert the response to a JSON object
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    user_data = user_response.json()
-    todo_data = todo_response.json()
-
-    employee_name = user_data["username"]
-    csv_file = "{}.csv".format(employee_id)
-
-    with open(csv_file, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todo_data:
-            writer.writerow([employee_id, employee_name, str(task["completed"]), task["title"]])
-
-    print("CSV file '{}' has been created successfully.".format(csv_file))
+    # Use list comprehension to iterate over the to-do list items
+    # Write each item's details (user ID, username, completion status,
+    #   and title) as a row in the CSV file
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
