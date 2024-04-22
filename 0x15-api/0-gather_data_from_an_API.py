5a3a2ea0781ce0a8ba70f3a2dbec3408b59b6cfe
ibrahim <ibrahimsaad6677@gmail.com>
2024-04-22 16:01:33 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
 """
-Script to retrieve and display information about an employee's TODO listprogres
+Returns to-do list information for a given employee ID.
+
+This script takes an employee ID as a command-line argument and fetches
+the corresponding user information and to-do list from the JSONPlaceholder API.
+It then prints the tasks completed by the employee.
 """
 
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
+    # Base URL for the JSONPlaceholder API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    employee_id = int(sys.argv[1])
+    # Get the employee information using the provided employee ID
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
 
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
-    todo_response = requests.get("{}/todos?userId={}".format(
-        base_url, employee_id))
+    # Get the to-do list for the employee using the provided employee ID
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params).json()
 
-    if user_response.status_code != 200:
-        print("Error: Could not retrieve user data")
-        sys.exit(1)
+    # Filter completed tasks and count them
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
 
-    if todo_response.status_code != 200:
-        print("Error: Could not retrieve TODO list data")
-        sys.exit(1)
-
-    user_data = user_response.json()
-    todo_data = todo_response.json()
-
-    employee_name = user_data["name"]
-    total_tasks = len(todo_data)
-    completed_tasks = [task["title"] for task in todo_data
-    if task["completed"]]
-
+    # Print the employee's name and the number of completed tasks
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(completed_tasks), total_tasks))
+        user.get("name"), len(completed), len(todos)))
 
-    for task_title in completed_tasks:
-        print("\t", task_title)
+    # Print the completed tasks one by one with indentation
+    [print("\t {}".format(complete)) for complete in completed]
