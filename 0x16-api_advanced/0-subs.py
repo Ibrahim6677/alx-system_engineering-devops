6538617c1917208c8493aed025098bd762e12e0e
ibrahim <ibrahimsaad6677@gmail.com>
2024-06-18 13:05:44 -0700

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,25 +3,36 @@
 Function that queries the Reddit API and returns
 the number of subscribers for a given subreddit.
 """
+
 import requests
 import sys
 
-
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
 
-    headers = {
-        'User-Agent': u_agent
-    }
+    Args:
+        subreddit (str): The name of the subreddit to query.
 
+    Returns:
+        int: The number of subscribers for the given subreddit, or 0 if an error occurs.
+    """
+    user_agent = 'Mozilla/5.0'
+    headers = {'User-Agent': user_agent}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code!= 200:
         return 0
-    dic = res.json()
-    if 'data' not in dic:
+    data = response.json()
+    if 'data' not in data or 'ubscribers' not in data.get('data'):
         return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+    return data['data']['subscribers']
+
+if __name__ == "__main__":
+    if len(sys.argv)!= 2:
+        print("Usage: python script.py <subreddit_name>")
+        sys.exit(1)
+    subreddit = sys.argv[1]
+    subscribers = number_of_subscribers(subreddit)
+    print(f"The subreddit {subreddit} has {subscribers} subscribers.")
+
