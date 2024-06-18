615c850d8cae27e0bba1145a4f0d4e1db5715ebf
ibrahim <ibrahimsaad6677@gmail.com>
2024-06-18 18:29:59 +0000

    finshed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,19 +9,19 @@ import requests
 import time
 
 def number_of_subscribers(subreddit):
-    u_agent = 'Mozilla/5.0'
-    headers = {'User-Agent': u_agent}
-    url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
-        return 0
-    time.sleep(1)  # add a 1-second delay to avoid rate limiting
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+	u_agent = 'Mozilla/5.0'
+	headers = {'User-Agent': u_agent}
+	url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
+	res = requests.get(url, headers=headers, allow_redirects=False)
+	if res.status_code != 200:
+		return 0
+	time.sleep(1)  # add a 1-second delay to avoid rate limiting
+	dic = res.json()
+	if 'data' not in dic:
+		return 0
+	if 'subscribers' not in dic.get('data'):
+		return 0
+	return res.json()['data']['subscribers']
 
 subreddit = "python"  # replace with the subreddit you want to query
 print(number_of_subscribers(subreddit))
