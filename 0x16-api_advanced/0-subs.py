5a1ed4c50884a7bb55afdb9a248dab9e3193c006
ibrahim <ibrahimsaad6677@gmail.com>
2024-06-18 18:32:45 +0000

    finshed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-
 """
 Function that queries the Reddit API and returns
 the number of subscribers for a given subreddit.
 """
 
 import requests
-import time
+import sys
+
 
 def number_of_subscribers(subreddit):
+	""" Queries to Reddit API """
 	u_agent = 'Mozilla/5.0'
-	headers = {'User-Agent': u_agent}
-	url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
+
+	headers = {
+		'User-Agent': u_agent
+	}
+
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 	res = requests.get(url, headers=headers, allow_redirects=False)
 	if res.status_code != 200:
 		return 0
-	time.sleep(1)  # add a 1-second delay to avoid rate limiting
 	dic = res.json()
 	if 'data' not in dic:
 		return 0
 	if 'subscribers' not in dic.get('data'):
 		return 0
 	return res.json()['data']['subscribers']
-
-subreddit = "python"  # replace with the subreddit you want to query
-print(number_of_subscribers(subreddit))
